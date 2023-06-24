3ebf5918e73dd75c3234395cab85d1f6fb48b0f3
sultaAann <sultandjavgashev@gmail.com>
2023-06-24 12:49:45 +0500

    ok

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -17,8 +17,8 @@ import java.util.List;
 public class SimpleJDBCRepository {
 
     private static final String DBNAME = "myfirstdb";
-    private static final String CREATE_USER_SQL = "INSERT INTO "+DBNAME + " (firstname, lastname, age) VALUES (?, ?, ?);";
-    private static final String UPDATE_USER_SQL = "UPDATE " +DBNAME + " SET firstname = ?, lastname = ?, age = ? WHERE id = ?";
+    private static final String CREATE_USER_SQL = "INSERT INTO " + DBNAME + " (firstname, lastname, age) VALUES (?, ?, ?);";
+    private static final String UPDATE_USER_SQL = "UPDATE " + DBNAME + " SET firstname = ?, lastname = ?, age = ? WHERE id = ?";
     private static final String DELETE_USER_SQL = "DELETE FROM " + DBNAME + " WHERE id = ?";
     private static final String FIND_USER_BY_ID_SQL = "SELECT * FROM " + DBNAME + " WHERE id = ?";
     private static final String FIND_USER_BY_NAME_SQL = "SELECT * FROM " + DBNAME + " WHERE firstname = ? OR lastname = ?";
@@ -27,53 +27,79 @@ public class SimpleJDBCRepository {
     private PreparedStatement ps = null;
     private Statement st = null;
 
-    public Long createUser(User user) throws SQLException, RuntimeException  {
-        ps = connection.prepareStatement(CREATE_USER_SQL);
-        ps.setString(1, user.getFirstName());
-        ps.setString(2, user.getLastName());
-        ps.setInt(3, user.getAge());
-        ps.execute();
+    public Long createUser(User user){
+        try {
+            ps = connection.prepareStatement(CREATE_USER_SQL);
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setInt(3, user.getAge());
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return user.getId();
     }
 
-    public User findUserById(Long userId) throws SQLException, RuntimeException {
-        ps = connection.prepareStatement(FIND_USER_BY_ID_SQL);
-        ps.setLong(1, userId);
-        ResultSet res = ps.executeQuery();
+    public User findUserById(Long userId) throws SQLException{
+        ResultSet res;
+        try {
+            ps = connection.prepareStatement(FIND_USER_BY_ID_SQL);
+            ps.setLong(1, userId);
+            res = ps.executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return new User(res.getLong(1), res.getString(2), res.getString(3), res.getInt(4));
     }
 
-    public User findUserByName(String userName) throws SQLException, RuntimeException  {
-        ps = connection.prepareStatement(FIND_USER_BY_NAME_SQL);
-        ps.setString(1, userName);
-        ps.setString(2, userName);
-        ResultSet res = ps.executeQuery();
+    public User findUserByName(String userName) throws SQLException {
+        ResultSet res;
+        try {
+            ps = connection.prepareStatement(FIND_USER_BY_NAME_SQL);
+            ps.setString(1, userName);
+            ps.setString(2, userName);
+            res = ps.executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return new User(res.getLong(1), res.getString(2), res.getString(3), res.getInt(4));
     }
 
-    public List<User> findAllUser() throws SQLException, RuntimeException  {
+    public List<User> findAllUser() {
         List<User> result = new ArrayList<>();
-        ps = connection.prepareStatement(FIND_ALL_USERS_SQL);
-        ResultSet res = ps.executeQuery();
-        while (res.next()) {
-            result.add(new User(res.getLong(1), res.getString(2), res.getString(3), res.getInt(4)));
+        try {
+            ps = connection.prepareStatement(FIND_ALL_USERS_SQL);
+            ResultSet res = ps.executeQuery();
+            while (res.next()) {
+                result.add(new User(res.getLong(1), res.getString(2), res.getString(3), res.getInt(4)));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
         return result;
     }
 
-    public User updateUser(User user) throws SQLException, RuntimeException  {
-        ps = connection.prepareStatement(UPDATE_USER_SQL);
-        ps.setString(1, user.getFirstName());
-        ps.setString(2, user.getLastName());
-        ps.setInt(3, user.getAge());
-        ps.setLong(4, user.getId());
-        ps.execute();
+    public User updateUser(User user) {
+        try {
+            ps = connection.prepareStatement(UPDATE_USER_SQL);
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setInt(3, user.getAge());
+            ps.setLong(4, user.getId());
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return user;
     }
 
-    public void deleteUser(Long userId) throws SQLException, RuntimeException  {
-        ps = connection.prepareStatement(DELETE_USER_SQL);
-        ps.setLong(1, userId);
-        ps.execute();
+    public void deleteUser(Long userId) {
+        try {
+            ps = connection.prepareStatement(DELETE_USER_SQL);
+            ps.setLong(1, userId);
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
